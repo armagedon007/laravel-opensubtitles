1cbbd0e434e48685c52bfb6657de3033a97f5ac5
armagedon007 <000yurik000@gmail.com>
2023-12-14 11:41:31 +0700

    Update HttpRequestInterface.php

--- src/HttpRequest/HttpRequestInterface.php
+++ src/HttpRequest/HttpRequestInterface.php
@@ -4,7 +4,20 @@ namespace CodeBugLab\OpenSubtitles\HttpRequest;
 
 interface HttpRequestInterface
 {
-    public function post(string $url, array $fields, array $header);
+    /**
+     * @param  string $url
+     * @param  array  $fields
+     * @param  array  $header
+     *
+     * @return null|string
+     */
+    public function post(string $url, array $fields, array $header): ?string;
 
-    public function get(string $url, array $header);
+    /**
+     * @param  string $url
+     * @param  array  $header
+     *
+     * @return null|string
+     */
+    public function get(string $url, array $header): ?string;
 }
