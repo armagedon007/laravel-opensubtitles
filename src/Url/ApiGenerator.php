c6039b5a2834700370e7be01bb0fd95b5bbad5e0
armagedon007 <000yurik000@gmail.com>
2023-12-14 11:52:42 +0700

    Update ApiGenerator.php

--- src/Url/ApiGenerator.php
+++ src/Url/ApiGenerator.php
@@ -12,33 +12,38 @@ class ApiGenerator implements ApiGeneratorInterface
 
     protected $apiKey;
 
+    protected $userAgent;
+
     protected $httpRequest;
 
-    public function __construct(string $apiUrl, string $apiKey, HttpRequestInterface $httpRequest)
+    public function __construct(string $apiUrl, string $apiKey, string $userAgent, HttpRequestInterface $httpRequest)
     {
         $this->apiUrl = $apiUrl;
 
         $this->apiKey = $apiKey;
 
+        $this->userAgent = $userAgent;
+
         $this->httpRequest = $httpRequest;
     }
 
-    public function post($path, $fields)
+    public function post(string $path, array $fields)
     {
         return $this->httpRequest->post($this->apiUrl . $path, $fields, $this->getHeaders());
     }
 
-    public function get($path)
+    public function get(string $path, array $parametrs)
     {
-        return $this->httpRequest->get($this->apiUrl . $path, $this->getHeaders());
+        return $this->httpRequest->get($this->apiUrl . $path, $parametrs, $this->getHeaders());
     }
 
 
     protected function getHeaders(): array
     {
         return [
-            "Content-type: application/json;",
-            "Api-Key: {$this->apiKey}"
+            'Content-type' => 'application/json',
+            'Api-Key' => $this->apiKey,
+            'User-Agent' => $this->userAgent
         ];
     }
 }
